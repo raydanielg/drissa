092c395780d373ee810a1cbff9999e6e69222140
raydanielg <airezra2+1@gmail.com>
2026-07-28 09:08:04 +0300

    commit

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -56,4 +56,9 @@ class Patient extends Model
     {
         return "{$this->first_name} {$this->last_name}";
     }
+
+    public function getNameAttribute(): string
+    {
+        return $this->fullName();
+    }
 }
