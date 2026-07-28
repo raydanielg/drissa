e4f69700715dfa9c8fa61700600ef53e5215665c
raydanielg <airezra2+1@gmail.com>
2026-07-28 10:26:35 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -7,8 +8,10 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/css/app.css',
             ],
             refresh: true,
         }),
+        tailwindcss(),
     ],
 });
